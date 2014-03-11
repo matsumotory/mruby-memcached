2f99f715e328f79d5edf358efa286fd0ada6adf1
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-03-11 21:22:19 +0900

    Fix test

--- test/mrb_memcached.rb
+++ test/mrb_memcached.rb
@@ -3,7 +3,7 @@
 ##
 
 assert("Memcached#set,get with string") do
-  m = Memcached.new "127.0.0.1", 11211
+  m = Memcached.new "127.0.0.1:11211"
   m.set "test", "1"
   real = m.get "test"
   m.close
@@ -11,7 +11,7 @@ assert("Memcached#set,get with string") do
 end
 
 assert("Memcached#set,get with symbol") do
-  m = Memcached.new "127.0.0.1", 11211
+  m = Memcached.new "127.0.0.1:11211"
   m.set :hoge, 10
   real = m.get :hoge
   m.close
@@ -19,7 +19,7 @@ assert("Memcached#set,get with symbol") do
 end
 
 assert("Memcached#set,get with symbol and string") do
-  m = Memcached.new "127.0.0.1", 11211
+  m = Memcached.new "127.0.0.1:11211"
   m.set "foo", 100
   real = m.get :foo
   m.close
