bafd2caec36c58ff6410ce4abafecf3570f24b8d
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-03-11 17:57:30 +0900

    Add test of symbol key

--- test/mrb_memcached.rb
+++ test/mrb_memcached.rb
@@ -2,10 +2,26 @@
 ## Memcached Test
 ##
 
-assert("Memcached#set,get") do
+assert("Memcached#set,get with string") do
   m = Memcached.new "127.0.0.1", 11211
   m.set "test", "1"
   real = m.get "test"
   m.close
   assert_equal("1", real)
+end
+
+assert("Memcached#set,get with symbol") do
+  m = Memcached.new "127.0.0.1", 11211
+  m.set :hoge, 10
+  real = m.get :hoge
+  m.close
+  assert_equal("10", real)
+end
+
+assert("Memcached#set,get with symbol and string") do
+  m = Memcached.new "127.0.0.1", 11211
+  m.set "foo", 100
+  real = m.get :foo
+  m.close
+  assert_equal("100", real)
 end
